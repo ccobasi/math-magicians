71ac8634c83b4181f4d43178f11c849a4ee8df4c
[Chukwuma Obasi] <ccobasi8@gmail.com>
2021-08-30 16:32:11 +0100

    deploy

---
+++ s/src/components/Home.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+const Home = () => (
+  <div className="home">
+    <h2 className="title">Welcome to our page!</h2>
+    <div className="row">
+      <div className="column">
+        <div className="card">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+          Officiis illo magnam placeat enim nobis voluptates vitae soluta!
+          Corrupti ad, minima laboriosam dolores nisi veniam, minus perspiciatis
+          ab, ipsa dolor officia.
+        </div>
+      </div>
+      <br />
+      <div className="column">
+        <div className="card">
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+          Officiis illo magnam placeat enim nobis voluptates vitae soluta!
+          Corrupti ad, minima laboriosam dolores nisi veniam, minus perspiciatis
+          ab, ipsa dolor officia.
+        </div>
+      </div>
+
+    </div>
+  </div>
+);
+
+export default Home;
---
+++ s/src/components/Quotes.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Quotes = () => (
+  <div className="quote">
+    <div className="row">
+      <div className="column">
+        <div className="card">
+          Mathematics is not about numbers, equations, computations, or algorithms:
+          it is about understanding. - William Paul Thurston
+        </div>
+      </div>
+    </div>
+  </div>
+);
+
+export default Quotes;
